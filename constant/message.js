38c00d54b51c5e95e0dfa0395f2448e8d5780a80
zyx <zyx@example.com>
2021-04-22 16:25:48 +0800

    v1.0

---
+++ constant/message.js
@@ -0,0 +1,8 @@
+// define the package type
+
+const SEND_PORT = 0;
+
+const DATA = 1;
+const CLOSE = 2;
+
+module.exports = { SEND_PORT, DATA, CLOSE };
---
+++ index.js
@@ -0,0 +1,7 @@
+const Server = require('./lib/server');
+const Client = require('./lib/client');
+
+module.exports = {
+    Server,
+    Client
+}
